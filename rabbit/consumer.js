c88f1e2900912b067ae06ae61721f018ea7312ae
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-10-19 06:58:40 +0200

    rabbits: log consume time

--- rabbit/consumer.js
+++ rabbit/consumer.js
@@ -2,13 +2,11 @@ var amqp = require('amqplib');
 var config = JSON.parse(process.argv[2]);
 
 var msgsToSend = config.msgsToSend;
-var start = new Date().getTime();
 
 amqp.connect('amqp://localhost').then(function(conn) {
   process.once('SIGINT', function() { conn.close(); });
   return conn.createChannel().then(function(ch) {
-
-    console.log('time (ms),latency (ms)');
+    console.log('time (ms),latency (ms), consume (ms)');
     var ok = ch.assertQueue(config.queue, config.queueOpts)
     .then(function() {
       return ch.purgeQueue(config.queue);
@@ -17,12 +15,25 @@ amqp.connect('amqp://localhost').then(function(conn) {
       return ch.prefetch(config.prefetchCount);
     })
     .then(function() {
-      return ch.consume(config.queue, function(msg) {
-        var now = new Date().getTime();
-        var then = 1*msg.content.toString();
-        console.log((now-start)+','+(now-then));
+      var start = Date.now();
+      var last;
 
-        setTimeout(function() {
+      return ch.consume(config.queue, function(msg) {
+        var now = Date.now();
+        var then = 1*msg.content.toString();
+        if (last === undefined) {
+          last = now;
+        }
+        console.log((now-start)+','+(now-then)+','+(now-last));
+        last = now;
+
+        if (config.msgAckDelay > 0) {
+          setTimeout(ack, config.msgAckDelay);
+        } else {
+          ack();
+        }
+
+        function ack() {
           if (! config.consumerOpts.noAck) {
             ch.ack(msg);
           }
@@ -30,7 +41,7 @@ amqp.connect('amqp://localhost').then(function(conn) {
           if (msgsToSend == 0) {
             setImmediate(function() {conn.close();});
           }
-        }, config.msgAckDelay);
+        }
       }, config.consumerOpts);
     });
   });
