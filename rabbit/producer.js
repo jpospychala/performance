e9b6208aacbf8cc89461145113c0f79f2ddbf8c7
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-10-19 06:57:07 +0200

    rabbits: log produce time

--- rabbit/producer.js
+++ rabbit/producer.js
@@ -6,15 +6,17 @@ var msgsToSend = config.msgsToSend;
 
 setTimeout(start, 1000);
 
+console.log('time (ms)');
 function start() {
     amqp.connect('amqp://localhost').then(function(conn) {
       return when(conn.createChannel().then(function(ch) {
         var q = config.queue;
-
         var ok = ch.checkQueue(q);
 
         return ok.then(function(_qok) {
           return when.promise(function(resolve, reject, notify) {
+            var start = Date.now();
+
             var intervalObj = setInterval(function() {
                 if (msgsToSend <= 0) {
                   clearInterval(intervalObj);
@@ -22,9 +24,11 @@ function start() {
                   return;
                 }
 
-                var msg = '' + (new Date().getTime());
+                var now = Date.now();
+                var msg = '' + (now);
                 msgsToSend--;
                 ch.sendToQueue(q, new Buffer(msg), config.msgOpts)
+                console.log(now-start);
             }, config.msgSendDelay);
           });
         });
