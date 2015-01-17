eeb809595636e925dbc2a810c46d57783fb9f31d
Jacek Pospychala <jacek.pospychala@gmail.com>
2015-01-17 21:08:05 +0100

    run apt-get update once

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -x
 echo digitalocean512 > ~/.perflabel
-apt-get update
-apt-get -y install git node.js gcc make docker.io npm g++ python-software-properties libzmq-dev
+apt-get -y install python-software-properties
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
+apt-get -y install git node.js gcc make docker.io npm g++ libzmq-dev
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get -y install oracle-java8-installer
